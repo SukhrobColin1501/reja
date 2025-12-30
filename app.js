d032a387dca39a5b78a25ce5f4e7a8ee58b00c76
SukhrobColin1501 <egamberdievsukhrob@gmail.com>
2025-12-31 02:29:06 +0500

    feat: mongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,54 @@
+console.log("Web Serverni Boshlash");
+const express = require ("express");
+const app = express();
+const fs = require("fs");
+
+
+// MongoDB call
+const db = require("./server").db();
+
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) =>{
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
+// 1 Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2 Session code 
+
+// 3 Views code 
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+// app.get("/hello", function(req, res) {
+//     res.send("<h1>HELLO WORLD by Colin</h1>");
+// });
+// app.get("/gift", function(req, res) {
+//     res.send("<h1>Siz sovgalar bo'limidasiz</h1>");
+// });
+app.post("/create-item", (req, res)=>{
+    console.log(req);
+    res.json({test: "success"});
+    // TODO: code with db here
+});
+// TODO code db here
+app.get('/author', (req, res) =>{
+    res.render("author", {user: user});
+
+});
+
+app.get("/", function (req, res) {
+    res.render("reja");
+});
+
+module.exports = app;
+
