638504708b91da34ee0b29f786f059cab6c94075
SukhrobColin1501 <egamberdievsukhrob@gmail.com>
2025-12-31 14:49:00 +0500

    feat: CRUD operatsiyalari

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const mongodb = require("mongodb");
 
 let db;
-const connectionString =" mongodb+srv://egamberdievsukhrob_db_user:RmxcK02h3T1tUCJ6@cluster0.umdu6s3.mongodb.net/Reja?appName=Cluster0"
+const connectionString ="mongodb+srv://egamberdievsukhrob_db_user:RmxcK02h3T1tUCJ6@cluster0.umdu6s3.mongodb.net/Reja"
 
 
 
