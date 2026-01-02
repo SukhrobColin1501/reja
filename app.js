a7ea6fee4480963010626412ee5725ddb804471f
SukhrobColin1501 <egamberdievsukhrob@gmail.com>
2026-01-02 14:22:02 +0500

    feat: FrontEnd database xato tugirlandi

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ res.json(data.ops [0]);
 app.post("/delete-item", (req, res)=> {
     const id = req.body.id;
     db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)},
-function (err, data){
+ function (err, data){
     res.json({state: "success"});
 } )
     // console.log(id);
